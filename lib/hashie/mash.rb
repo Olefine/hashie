a8ca2cb6e661666f22d5216e2986ffae141a86f6
Olefine <egor.gorodov@gmail.com>
2013-11-02 12:55:39 +0400

    mash realization without refactoring

--- lib/hashie/mash.rb
+++ lib/hashie/mash.rb
@@ -1,4 +1,20 @@
 module Hashie
   class Mash
+    def initialize()
+      @hash = {}
+    end
+
+    def method_missing(name, *args)
+      mod = name[-1]
+      key = name.to_s.delete(mod).to_sym
+      if mod == '?'
+        @hash.has_key?(key)
+      elsif mod == '='
+        @hash[key] = args[0]
+      else
+        key = name
+        @hash[key]
+      end
+    end
   end
 end
--- test/hashie/mash_test.rb
+++ test/hashie/mash_test.rb
@@ -5,4 +5,15 @@ class MashTest < MiniTest::Test
     hash = Hashie::Mash.new
     assert hash != nil
   end
+
+  def test_responders_attrs
+    mash = Hashie::Mash.new
+    assert_equal false, mash.name?
+    assert_equal nil, mash.name
+
+    mash.name = "My Mash"
+
+    assert_equal "My Mash", mash.name
+    assert_equal true, mash.name?
+  end
 end
